9874bf58741d1761b1374bf81e22c4d4f2c1eb12
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-07-02 20:38:22 +0700

    have a scss

---
+++ src/assets/js/Bar.js
@@ -0,0 +1,7 @@
+const iconBar = document.querySelector('.icon-bar');
+const navBar = document.querySelector('.navbar');
+
+iconBar.onclick = () => {
+    // iconBar.classList.toggle('fa-times');
+   navBar.classList.toggle('active')
+}
